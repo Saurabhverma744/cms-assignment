393f75da5db1410a3074b3140a02fe2f0cda8998
SaurabhVerma744 <saurabhverma0595@gmail.com>
2026-07-25 04:40:25 +0530

    Update frontend API URL for production

--- admin-frontend/src/services/api.js
+++ admin-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5002/api",
+  baseURL: "https://saurabh-cms-backend.onrender.com/api",
 });
 
 export default api;
